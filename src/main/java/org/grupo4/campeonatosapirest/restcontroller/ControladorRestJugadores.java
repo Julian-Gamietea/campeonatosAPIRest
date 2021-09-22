e5ff0b15558c181686be933f688053a40aeff3bf
PazGonzalo <89784467+PazGonzalo@users.noreply.github.com>
2021-09-22 18:09:46 -0300

    ControladorRestJugadores :D

--- src/main/java/org/grupo4/campeonatosapirest/restcontroller/ControladorRestJugadores.java
+++ src/main/java/org/grupo4/campeonatosapirest/restcontroller/ControladorRestJugadores.java
@@ -1,4 +1,113 @@
 package org.grupo4.campeonatosapirest.restcontroller;
 
+
+import org.grupocuatro.controlador.ControladorJugadores;
+import org.grupocuatro.modelo.Jugador;
+import org.grupocuatro.vo.JugadorVO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
 public class ControladorRestJugadores {
+
+    @PostMapping("/crearJugador")
+    public Integer crearJugador(@RequestBody JugadorVO jugador) {
+        return ControladorJugadores.getInstancia().agregarJugador(jugador.getTipoDocumento(), jugador.getDocumento(), jugador.getNombre(), jugador.getApellido(), jugador.getClub().getIdClub(), jugador.getFechaNacimiento(), jugador.getDireccion(), jugador.getMail(), jugador.getTelefono());
+    }
+
+    @PostMapping("/modificarDireccion")
+    public void modificarDireccion(@RequestParam(name = "idJugador") Integer idJugador, @RequestParam(name = "direccion") String direccion) {
+        ControladorJugadores.getInstancia().modificarDireccion(idJugador, direccion);
+    }
+
+    @PostMapping("/modificarMail")
+    public void modificarMail(@RequestParam(name = "idJugador") Integer idJugador, @RequestParam(name = "mail") String mail) {
+        ControladorJugadores.getInstancia().modificarMail(idJugador, mail);
+    }
+
+    @PostMapping("/modificarTelefono")
+    public void modificarTelefono(@RequestParam(name = "idJugador") Integer idJugador, @RequestParam(name = "telefono") String telefono) {
+        ControladorJugadores.getInstancia().modificarTelefono(idJugador, telefono);
+    }
+
+    @RequestMapping("/modificarEstado")
+    public void modificarEstado(@RequestParam(name = "idJugador") Integer idJugador) {
+        ControladorJugadores.getInstancia().modificarEstado(idJugador);
+    }
+
+    @RequestMapping("/getStatsByCampeonato")
+    public void getStatsByCampeonato(@RequestParam(name = "idJugador") Integer idJugador, @RequestParam(name = "idCampeonato") Integer idCampeonato) {
+        ControladorJugadores.getInstancia().getStatsByCampeonato(idJugador, idCampeonato);
+    }
+
+    @RequestMapping("/getStatsByClub")
+    public void getStatsByClub(@RequestParam(name = "idJugador") Integer idJugador, @RequestParam(name = "idClub") Integer idClub) {
+        ControladorJugadores.getInstancia().getStatsByClub(idJugador, idClub);
+    }
+
+    @RequestMapping("/getJugadores")
+    public List<JugadorVO> getJugadores() {
+        List<Jugador> jugadores = ControladorJugadores.getInstancia().getJugadores();
+        List<JugadorVO> jugadoresVO = Collections.emptyList();
+        for (Jugador jugador : jugadores) {
+            jugadoresVO.add(jugador.toVO());
+        }
+        return jugadoresVO;
+    }
+
+    @RequestMapping("/getJugadoresByClub")
+    public List<JugadorVO> getJugadoresByClub(@RequestParam(name = "idClub") Integer idClub) {
+        List<Jugador> jugadores = ControladorJugadores.getInstancia().getJugadoresByClub(idClub);
+        List<JugadorVO> jugadoresVO = Collections.emptyList();
+        for (Jugador jugador : jugadores) {
+            jugadoresVO.add(jugador.toVO());
+        }
+        return jugadoresVO;
+    }
+
+
+    @RequestMapping("/getJugadoresByCategoria")
+    public List<JugadorVO> getJugadoresByCategoria(@RequestParam(name = "categoria") int categoria) {
+        List<Jugador> jugadores = ControladorJugadores.getInstancia().getJugadoresByCategoria(categoria);
+        List<JugadorVO> jugadoresVO = Collections.emptyList();
+        for (Jugador jugador : jugadores) {
+            jugadoresVO.add(jugador.toVO());
+        }
+        return jugadoresVO;
+    }
+
+
+    @RequestMapping("/getJugadoresHabilitadosByClubAndCategoria")
+    public List<JugadorVO> getJugadoresHabilitadosByClubAndCategoria(@RequestParam(name = "idClub") Integer idClub, @RequestParam(name = "categoria") int categoria) {
+        List<Jugador> jugadores = ControladorJugadores.getInstancia().getJugadoresHabilitadosCategoriaClub(idClub, categoria)
+        List<JugadorVO> jugadoresVO = Collections.emptyList();
+        for (Jugador jugador : jugadores) {
+            jugadoresVO.add(jugador.toVO());
+        }
+        return jugadoresVO;
+    }
+
+
+
+    @RequestMapping("/getJugadorByDocumento")
+    public JugadorVO getJugadorByDocumento(@RequestParam(name = "documento") Integer documento, @RequestParam(name = "tipoDoc") String tipoDoc) {
+        return ControladorJugadores.getInstancia().getJugadorByDocumento(documento, tipoDoc).toVO();
+    }
+
+
+    @RequestMapping("/getJugadorByNombre")
+    public List<JugadorVO> getJugadorByNombre(@RequestParam(name = "nombre") String nombre, @RequestParam(name = "apellido") String apellido) {
+        List<Jugador> jugadores = ControladorJugadores.getInstancia().getJugadorByNombre(nombre, apellido);
+        List<JugadorVO> jugadoresVO = Collections.emptyList();
+        for (Jugador jugador : jugadores) {
+            jugadoresVO.add(jugador.toVO());
+        }
+        return jugadoresVO;
+    }
+
+
+
+
 }
