9406cfa3b49e1802e3f885aa57be7cad2713f6a9
agussturba <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>
2021-09-22 19:04:11 -0300

    ElmejorDeClubes

--- src/main/java/org/grupo4/campeonatosapirest/restcontroller/ControladorRestClubes.java
+++ src/main/java/org/grupo4/campeonatosapirest/restcontroller/ControladorRestClubes.java
@@ -18,19 +18,19 @@ public class ControladorRestClubes {
         ControladorClubes.getInstancia().modificarClub(idClub, nombre, direccion);
     }
 
-    @GetMapping("/getClubById")
+    @RequestMapping("/getClubById")
     public void getClubById(@RequestParam(name = "idClub") Integer idClub ){
         ControladorClubes.getInstancia().getClubById(idClub);
     }
-    @GetMapping("/getClubes")
+    @RequestMapping("/getClubes")
     public void getClubes(){
         ControladorClubes.getInstancia().getClubes();
     }
-    @GetMapping("/getClubByCampeonato")
+    @RequestMapping("/getClubByCampeonato")
     public void getClubByCampeonato(@RequestBody Campeonato campeonato){
         ControladorClubes.getInstancia().getClubesByCampeonato(campeonato.getIdCampeonato());
     }
-    @GetMapping("/getClubesHabiltadosPorCategoria")
+    @RequestMapping("/getClubesHabiltadosPorCategoria")
     public void getClubesHabiltadosPorCategoria(@RequestParam(name = "Categoria") int categoria){
         ControladorClubes.getInstancia().getClubesHabiltadosPorCategoria(categoria);
     }
